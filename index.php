a08f04c095cd3daac6532ad9f8a8f5576800d579
//sara <44452973+frenchfri@users.noreply.github.com>
2019-04-17 21:21:37 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>form</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<?php require_once 'process.php'; ?>
+
+	<?php
+	if (isset($_SESSION['message'])): ?>
+	<div class="alert alert-<?=$_SESSION['msg_type']?>">
+
+	<?php
+	echo $_SESSION['message'];
+	unset($_SESSION['message']);	
+	?>
+	</div>
+	<?php endif ?>
+
+
+
+	<div class="container">
+
+	<?php
+		$mysqli = new mysqli('localhost', 'root', '', 'crud') or die (mysqli_error($mysqli));
+		$result = $mysqli->query("SELECT * FROM data");
+		// pre_r($result);
+		// pre_r($result->fetch_assoc());
+		// pre_r($result->fetch_assoc());
+		?>
+
+		<div class="row justify-content-center">
+
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Location</th>
+						<th colspan="2">Action</th>
+					</tr>
+				</thead>
+		<?php
+			while ($row = $result->fetch_assoc()): ?>
+				<tr>
+					<td><?php echo $row['name']; ?> </td>
+					<td><?php echo $row['location']; ?> </td>
+					<td>
+				
+							<a href="index.php?edit=<?php echo $row['id']; ?>"	
+							class="btn btn-info">Edit</a>
+							<a href="process.php?delete=<?php echo $row['id']; ?>"
+							class="btn btn-danger">Delete</a>
+<					</td>
+				</tr>
+		<?php endwhile; ?>
+			</table>
+		</div>
+
+		
+
+<?php
+		function pre_r($array){
+			echo '<pre>';
+			print_r($array);
+			echo '</pre>';
+
+		}
+	?>
+	<div class="row justify-content-center">
+<form action="process.php" method="POST">
+  <input type="hidden" name="id" value="<?php echo $id; ?>">
+  <div class="form-group">
+  <label>Name</label>
+  <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" 
+  				placeholder="enter your name" >
+  </div>
+  <div class="form-group">        
+  <label>Location</label>
+  <input type="text" name="location"class="form-control" value="<?php echo $location; ?>" placeholder="enter your location">
+  </div>
+  <div class="form-group">
+	  <?php
+	  if ($update == true):
+
+	   ?>
+  	      <button type="submit" class="btn btn-primary" name="update">Update</button>
+       <?php else: ?>
+		 <button type="submit" class="btn btn-primary" name="save">Save</button>
+	  <?php endif; ?>
+  </div>   
+</form>
+</div>
+</div>
+</body>
+
+
+</html>
